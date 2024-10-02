558928c7c17f21fd4cf4fc3529e6458ab9212e63
Ask <villeravn@gmail.com>
2024-10-02 10:36:48 +0200

    ferdig

--- andregradslikning.py
+++ andregradslikning.py
@@ -1,6 +1,22 @@
-# Finner løsninger til alle andregradslikninger på formen ax^2+bx+c=0
 import math
 
 def losninger(a, b, c):
-  # Din kode her
-  return #Returneringsverdi
+  #skjekk antall losninger
+  diskriminant = b**2 - 4*a*c
+  
+  #returner riktige antall verdier basert på diskriminanten
+  if diskriminant < 0:
+    return "Ingen losninger"
+  
+  losning1 = (-b - math.sqrt(diskriminant)) / (2*a)
+  round(losning1,2)
+  
+  losning2 = (-b + math.sqrt(diskriminant)) / (2*a)
+  round(losning2,2)
+
+  if diskriminant == 0:
+
+    return losning1
+  
+  elif diskriminant > 0:
+    return (losning1, losning2)
